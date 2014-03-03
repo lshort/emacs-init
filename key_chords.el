f01b6de2fe93a405f8701b1849b0ed873ff08d25
Lee Short <blackcat@pro-ns.net>
2014-03-02 19:41:13 -0500

    more key chord customizations

--- key_chords.el
+++ key_chords.el
@@ -4,11 +4,13 @@
 (require 'key-chord)
 (key-chord-mode 1)
 
+;;  q chords
+;;
 (key-chord-define-global "qy" 'ido-find-file)
 (key-chord-define-global "qi" 'split-window-below)
 (key-chord-define-global "qo" 'split-window-right)
 (key-chord-define-global "qp" 'delete-window)
-(key-chord-define-global "q]" 'delete-other-windows)
+(key-chord-define-global "q[" 'delete-other-windows)
 (key-chord-define-global "q]" 'shrink-window-3-lines) ;; or 33%?
 
 (key-chord-define-global "qh" 'goto-line)
@@ -20,10 +22,12 @@
 
 (key-chord-define-global "qn" 'recenter-top-bottom)  ;; control-L
 ;(key-chord-define-global "qm" 'hippie-expand)
-(key-chord-define-global "q," 'revert-buffer)
+;(key-chord-define-global "q," 'revert-buffer)
 (key-chord-define-global "q." 'narrow-to-region)
 (key-chord-define-global "q/" 'widen)
 
+;;  ; chords
+;;
 (key-chord-define-global ";q" 'kill-region)
 (key-chord-define-global ";w" 'kill-ring-save)
 (key-chord-define-global ";e" 'yank)
@@ -42,5 +46,37 @@
 (key-chord-define-global ";v" 'isearch-forward)
 ;(key-chord-define-global ";b" ')
 
+
+;;  all-left-hand chords
 ;;
-;;; init.el ends here
+;(key-chord-define-global "qw" 'xx)
+(key-chord-define-global "we" 'sp-backward-kill-word)
+(key-chord-define-global "er" 'sp-kill-word)
+
+(key-chord-define-global "as" 'prelude-move-beginning-of-line)
+(key-chord-define-global "sd" 'left-word)
+(key-chord-define-global "df" 'right-word)
+(key-chord-define-global "fg" 'move-end-of-line)
+
+(key-chord-define-global "xc" 'undo)
+;(key-chord-define-global "cv" 'isearch-forward)
+
+
+;;  all-right-hand chords
+;;
+(key-chord-define-global "ui" 'xx)
+(key-chord-define-global "io" 'sp-backward-kill-word)
+(key-chord-define-global "op" 'sp-kill-word)
+
+(key-chord-define-global "hj" 'left-char)
+(key-chord-define-global "jk" 'previous-line)
+(key-chord-define-global "kl" 'next-line)
+(key-chord-define-global "l;" 'right-char)
+
+(key-chord-define-global ",." 'isearch-forward)
+(key-chord-define-global "m," 'undo)
+(key-chord-define-global ",." 'isearch-forward)
+
+
+;;
+;;; key_chords.el ends here
